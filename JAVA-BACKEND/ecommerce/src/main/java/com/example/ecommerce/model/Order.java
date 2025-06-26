6bb2a2eda4e69bc5f3981c6df77ab492f4e48258
Ru5hic10 <rushalchavan1059@gmail.com>
2025-06-26 23:56:24 +0530

    order backend

---
+++ JAVA-BACKEND/ecommerce/src/main/java/com/example/ecommerce/model/Order.java
@@ -0,0 +1,26 @@
+package com.example.ecommerce.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Data
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private double amount;
+
+    private LocalDateTime orderTime;
+
+    @ElementCollection
+    private List<String> productNames;
+
+    private String status; // "PAID"
+}
